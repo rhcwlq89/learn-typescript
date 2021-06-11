dda2347564325282354a4623a8ef85b5c636f2d3
이호종 <ccaja21@naver.com>
2021-06-11 19:24:21 +0900

    메모추가

---
+++ getting-started/index.ts
@@ -0,0 +1,16 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 컴파일이 필요함. (ts -> js)
+/**
+ * 컴파일을 위해서 node 버전
+ * npm i typescipt -g
+ * 사용법 (tsc)
+ * tsc index.ts
+ * 
+ * 웹 태스크 매니져 (webpack)을 쓰면 tsc 하는 것보다 편해짐.
+ */
+
